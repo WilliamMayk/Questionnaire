3c26561a6d8e16ca92ed778f1361313053ec9d4d
Okwale Mayoukou William <william.okwale@gmail.com>
2023-11-20 16:05:35 +0000

    Create form.js

---
+++ form.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATfGoWCBIbBMxiAk79vCfB4Kzt5xqmwBE",
+    authDomain: "questionnaire-form-746af.firebaseapp.com",
+    databaseURL: "https://questionnaire-form-746af-default-rtdb.firebaseio.com",
+    projectId: "questionnaire-form-746af",
+    storageBucket: "questionnaire-form-746af.appspot.com",
+    messagingSenderId: "952496296623",
+    appId: "1:952496296623:web:b40877cd35a2a9a7357a5a"
+  };
