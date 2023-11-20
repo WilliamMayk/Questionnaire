b600b9143fc8dcd9cdb545c039055b8554c30c21
Okwale Mayoukou William <william.okwale@gmail.com>
2023-11-20 16:35:30 +0000

    update

--- form.js
+++ form.js
@@ -7,3 +7,31 @@ const firebaseConfig = {
     messagingSenderId: "952496296623",
     appId: "1:952496296623:web:b40877cd35a2a9a7357a5a"
   };
+
+  //initialize firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //reference the database
+  var questionnaireFormDB = firebase.database().ref('questionnaireForm');
+
+  document.getElementById('questionnaireForm').addEventListener("submit",submitForm )
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    var industry = getElementVal("companyName");
+    var address = getElementVal("inputAddress");
+    var person = getElementVal("inputPerson");
+    var email = getElementVal("inputEmail");
+    var phoneNumber = getElementVal("inputPhoneNumber");
+    var url = getElementVal("inputUrl");
+    var socialProfiles = getElementVal("inputSocialProfiles");
+    var onlineMarketplaces = getElementVal("onlineMarketplaces");
+    var KPI = getElementVal("inputKPI");
+    var option1 = getElementVal("flexOption1");
+    var option2 = getElementVal("flexOption2");
+  }
+
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  }
